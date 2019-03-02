0ead3083919dd030a2512853a40d7a3761ad88e2
Patrick A <akolo@hotmail.com>
2019-03-02 14:09:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 def countdown (counter)
-  counter > 0
-  while counter > 1
+  counter = 10
+  while counter > 0
   puts "#{counter} SECOND(S)!"
   counter -= 1
   end
@@ -9,8 +9,8 @@ def countdown (counter)
 end
 
 def countdown_with_sleep(counter)
-  counter > 0
-  while counter > 1
+  counter = 10
+  while counter > 0
   puts "#{counter} SECOND(S)!"
 	counter -= 1
   sleep(1.second)
